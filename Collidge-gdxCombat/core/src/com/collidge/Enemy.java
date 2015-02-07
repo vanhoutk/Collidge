eb60c5a29d1fbfc7b2c21c08256f7cfb4c6ceb9c
Michael Stroughair <stroughm@tcd.ie>
2015-02-07 16:21:15 +0000

    Added some shit

---
+++ Collidge-gdxCombat/core/src/com/collidge/Enemy.java
@@ -0,0 +1,90 @@
+package com.collidge;
+
+import java.util.Random;
+
+/**
+ * Created by Daniel on 21/01/2015.
+ */
+public class Enemy
+{
+    private int maxHealth,currentHealth,attack,defence;
+    private boolean dead;
+    String name;
+
+    private Random rand=new Random();
+    public int getExpValue()
+    {
+        return expValue;
+    }
+
+    private int expValue;
+
+    Enemy(String nm, int h, int a, int d,int exp)
+    {
+        name=nm;
+        maxHealth=h;
+        attack=a;
+        defence=d;
+        expValue=exp;
+        currentHealth=maxHealth;
+        dead=false;
+    }
+
+    Enemy(Enemy template)
+    {
+
+        maxHealth=template.maxHealth;
+        attack=template.attack;
+        defence=template.getDefence();
+        expValue=template.getExpValue();
+        name=template.getName();
+
+        maxHealth+=(int)(maxHealth*(rand.nextDouble()-.5));
+        attack+=(int)(attack*(rand.nextDouble()-.5));
+        defence+=(int)(defence*(rand.nextDouble()-.5));
+        expValue+=(int)(expValue*(rand.nextDouble()-.5));
+        currentHealth=maxHealth;
+
+        System.out.println(this.getName()+": "+this.maxHealth+"hp -ATK:"+this.attack+"  DEF:"+this.defence+"  EXP:"+this.expValue);
+        dead=false;
+
+
+
+    }
+
+    public boolean getDead()
+    {
+        return dead;
+    }
+    public int getHealth()
+    {
+        return currentHealth;
+    }
+    public int getDefence()
+    {
+        return defence;
+    }
+    public int getAttack()
+    {
+        return attack;
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+    public void changeHealth(int dH)
+    {
+        currentHealth+=dH;
+        if(currentHealth>maxHealth)
+        {
+            currentHealth=maxHealth;
+        }
+        else if(currentHealth<=0)
+        {
+            dead=true;
+        }
+    }
+
+
+}
---
+++ Collidge-gdxCombat/core/src/com/collidge/GameState.java
@@ -0,0 +1,42 @@
+package com.collidge;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+
+/**
+ * Created by Ben on 24-Jan-15.
+ *
+ * Abstract gameState which all other gameStates inherit off. Abstract meaning that it can't be made into an object and that states that inherit off it must implement every function.
+ * Has a reference to the gameStateManager so it can swap to any other state at any time using the gsm's public functions.
+ */
+public abstract class GameState extends ApplicationAdapter
+{
+    //reference to the gsm so can change to any other at any time. if (anything) gsm.changeState(state);
+    protected GameStateManager gsm;
+    int screenWidth, screenHeight;
+
+    //constructor assigns gsm to arguments gsm so every gameState has same gsm.
+    GameState(GameStateManager gsm)
+    {
+        this.gsm = gsm;
+        screenWidth= Gdx.graphics.getWidth();
+        screenHeight= Gdx.graphics.getHeight();
+    }
+
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    //6 FUNCTIONS THAT EVERY GAMESTATE MUST IMPLEMENT. EVEN IF COMPLETELY EMPTY!
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    //initialize is the same as constructor but sometimes you want to reset an object without rebuilding it.
+    public abstract void initialize();
+    //update will update all game logic before drawing
+    public abstract void update();
+    //after updating has occured in that single frame. Both what or what hasn't been changed has to be drawn/redrawn
+    public abstract void draw();
+
+    //3 touch events that you can handle inside your own class
+    public abstract boolean touchDown(int screenX, int screenY, int pointer, int button);
+    public abstract boolean touchUp(int screenX, int screenY, int pointer, int button);
+    public abstract boolean touchDragged(int screenX, int screenY, int pointer);
+}
