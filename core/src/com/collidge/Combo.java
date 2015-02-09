3e63b666e045852c8f0aef7762a10fb3623318a5
daniel11235 <mccormd2@tcd.ie>
2015-02-09 14:57:57 +0000

    Target picker and music!

--- core/src/com/collidge/Combo.java
+++ core/src/com/collidge/Combo.java
@@ -5,11 +5,14 @@ package com.collidge;
  */
 public class Combo
 {
+    double startTime;
+    double startX,startY;
+    double endX,endY;
     Combo()
     {
 
     }
-    double initiateCombo(int moveId)
+    double initiateCombo(int moveId,Fight fight)
     {
         switch(moveId)
         {
@@ -32,8 +35,15 @@ public class Combo
     double swipe(int dx, int dy,int x1,int y1,int x2,int y2)
     {
         //TODO add in swipe code
+        
         return 0;
     }
+
+    void touchDown(int x,int y)
+    {
+
+    }
+
 
 }
 
