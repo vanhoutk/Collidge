80a50ef7d50844d24ea3d042fb64d073c9f7d9f9
dcricci <1dcricci1@gmail.com>
2015-03-30 02:10:06 +0100

    Map loading adjusting [fixing weird window bug]

--- core/src/com/collidge/Play.java
+++ core/src/com/collidge/Play.java
@@ -189,6 +189,8 @@ public class Play extends GameState {
         renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("ground"));
         renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("walls"));
         renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("front_fence"));
+        renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("back_fence"));
+        renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("tree1"));
         renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("randoms_layer"));
 
         if(TimeUtils.timeSinceMillis(enteringFight)>3000)
@@ -201,8 +203,6 @@ public class Play extends GameState {
         }
 
         //Loads the rest of the map on top of the NPC and Player models
-        renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("back_fence"));
-        renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("tree1"));
         renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("tree2_and_windows"));
         renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("roof"));
         renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("roof2"));
